458274dc6b751aba9c8f29dfebad9d9898b6aa73
aasli <aasli@ASLINB>
2023-10-02 00:34:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string result = Compare(3,3);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+
+    public static string Compare(int A, int B)
+    {
+        if (A > B)
+        {
+            return "A büyüktür B";
+        } else if (B > A) {
+            return "B büyüktür A";
+        } else
+        {
+            return "A B'ye eşittir.";
+        }
+       
+    }
+    
+
+
+
+}
